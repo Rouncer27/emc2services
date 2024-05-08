388ac5e077eec5ff5c68d8b19379c5c44a6f7d1b
Trevor Rounce <trevor@switchbackcreative.ca>
2024-05-08 12:33:25 -0600

    init setup of the website

---
+++ src/data/siteData.js
@@ -0,0 +1,9 @@
+import defaultSrc from "../../public/default.jpg";
+
+export const baseData = {
+  title: "e=mc2",
+  description:
+    "e=mc2 - .The Centre For Business Excellence, where Ideas Are Formed",
+  siteImg: `https://emc2services.netlify.app/${defaultSrc.src}`,
+  url: "https://emc2services.ca/",
+};
