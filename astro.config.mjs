a98fd6cd1b030b36f322ec38092c13e7ace17934
Trevor Rounce <trevor@switchbackcreative.ca>
2024-07-02 11:24:49 -0600

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), sitemap()],
   image: {
-    domains: ["emc2services.swbdatabases3.com"],
-  },
+    domains: ["emc2services.swbdatabases3.com"]
+  }
 });
