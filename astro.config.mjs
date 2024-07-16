97cf480220ad03b8bde59eb708756e9327e4dad2
Trevor Rounce <trevor@switchbackcreative.ca>
2024-07-16 09:48:39 -0600

    added Google analytics to website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,21 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import partytown from "@astrojs/partytown";
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://emc2services.ca/",
-  integrations: [react(), sitemap()],
+  integrations: [
+    react(),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   image: {
     domains: ["emc2services.swbdatabases3.com"],
   },
